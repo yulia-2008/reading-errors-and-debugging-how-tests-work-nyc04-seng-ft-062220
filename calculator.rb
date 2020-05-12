77f6c35bd8f9f6710ac1fd25ff11dcc10fb1e622
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-12 19:15:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
-def calculator
-  first_number = "h"
+
+  first_number = 1
   second_number = 2
   sum = first_number + second_number
   difference = first_number - second_number
   product = first_number*second_number
   quotient = first_number/second_number
-end 
+
