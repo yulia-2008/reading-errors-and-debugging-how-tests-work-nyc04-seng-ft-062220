1a5e57754e61b3d079357d35c5299500e7464670
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-12 19:11:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,5 @@ def calculator
   sum = first_number + second_number
   difference = first_number - second_number
   product = first_number*second_number
+  quotient = first_number/second_number
 end 
